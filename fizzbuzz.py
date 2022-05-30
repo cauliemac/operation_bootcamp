d225a79e130a20c2ac36b24173db215003351d65
cauliemac <39170797+cauliemac@users.noreply.github.com>
2022-05-30 22:56:54 +0100

    Create fizzbuzz.py
    created fizzbuzz file

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 30 22:51:45 2022
+
+@author: cauli
+
+Given a positive integer A, return an array of strings with all the integers
+from 1 to N. 
+But for multiples of 3 the array should have “Fizz” instead of the number. 
+For the multiples of 5, the array should have “Buzz” instead of the number. 
+For numbers which are multiple of 3 and 5 both, the array should have “FizzBuzz”
+instead of the number.
+
+"""
+
+import string
