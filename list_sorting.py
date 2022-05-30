b99fd34c236bf724f32e9327464418eec0f5ef85
cauliemac <39170797+cauliemac@users.noreply.github.com>
2022-05-30 22:53:08 +0100

    Initial commit

---
+++ list_sorting.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 30 22:01:23 2022
+
+@author: cauli
+
+mewssing with lists
+"""
+import string
+import random
+
+def rand_string_func(length_word):  
+    rand_string = "" 
+    for i in range(length_word):
+        rand_choice = random.choice(string.ascii_lowercase)
+        rand_string += rand_choice 
+    return rand_string
+
+def unsorted_list_func(length_list, length_word):
+    list_1 = []
+    for i in range(length_list):
+        list_1.append(rand_string_func(length_word))
+    return list_1
+
+def list_sorter_func(unsorted_list):
+    unsorted_list.sort()
+    sorted_list = unsorted_list
+    return sorted_list
+
+var_unsorted_list = unsorted_list_func(5,3)
+
+var_sorted_list = list_sorter_func(var_unsorted_list)
+  
+print(var_unsorted_list)
+print(var_sorted_list)
