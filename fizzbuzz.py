b21d82059f66fa4d11757fe50ed7c140ff659fca
cauliemac <39170797+cauliemac@users.noreply.github.com>
2022-05-30 23:34:14 +0100

    made fizzbuzz a function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -12,25 +12,24 @@ For numbers which are multiple of 3 and 5 both, the array should have “FizzBuz
 instead of the number.
 
 """
-#string_N = input("Please enter the number of iterations: ")
-string_N = 15
-int_1 = 3
-int_2 = 5
 
-int_N= int(string_N)
-word_1 = "Fizz"
-word_2 = "Buzz"
+def Fizzbuzz(N, int_1, int_2): 
+    word_1 = "Fizz"
+    word_2 = "Buzz"
+    
+    for i in range(N):
+        count = i+1
+        
+        if count%int_2==0 and count%int_1==0:
+            print(word_1+word_2)
+            continue
+        if count%int_1 ==0:
+            print(word_1)
+        if count%int_2==0:
+            print(word_2)
+        else:
+            print(count)
+        
+    return
 
-for i in range(int_N):
-    count = i+1
-    
-    if count%int_2==0 and count%int_1==0:
-        print(word_1+word_2)
-        break
-    if count%int_1 ==0:
-        print(word_1)
-    if count%int_2==0:
-        print(word_2)
-    
-    else:
-        print(count)
+Fizzbuzz(50,3,5)
